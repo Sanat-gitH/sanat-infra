5351fd08987a03856707440a0027e949d4c6c409
Sanat-gitH <sannatgiri1@gmail.com>
2026-08-20 04:18:15 +0530

    Update azurerm provider version constraint

--- provider.tf
+++ provider.tf
@@ -1,13 +1,12 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "4.0.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
   }
 }
+
 provider "azurerm" {
-    feature {}
-    subscription_id = "d65eae7c-26c4-4428-ad14-562dd1d1083d"
-  
+  features {}
 }
